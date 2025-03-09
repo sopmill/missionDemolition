d3984c25035c41cd72924283672fd5ceeccf74f1
sopmill <156019017+sopmill@users.noreply.github.com>
2025-03-09 18:04:13 -0500

    MORE

---
+++ Mission Demolition/Assets/WelcomeScreen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class WelcomeScreen : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("_Scene_0"); 
+    }
+}
+
